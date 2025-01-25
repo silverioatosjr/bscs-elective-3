14f97aa1ead1ede22a509fbbbd1bd60ffa6f5639
satos <satos@wylog.com>
2025-01-25 15:59:43 +0800

    Added contact page

---
+++ frontend/app/courses/bscs/page.tsx
@@ -0,0 +1,7 @@
+export default function BSCS() {
+    return (
+        <div>
+            <h1>BS Computer Science</h1>
+        </div>
+    )
+}
---
+++ frontend/app/courses/bsis/page.tsx
@@ -0,0 +1,7 @@
+export default function BSIS() {
+    return (
+        <div>
+            <h1>BS Information System</h1>
+        </div>
+    )
+}
