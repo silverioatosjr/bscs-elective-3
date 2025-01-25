9fd243ae190042daeef056c8153bb9c169652c74
satos <satos@wylog.com>
2025-01-25 14:04:09 +0800

    Initial commit

--- client/app/(auth)/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default async function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex justify-center items-center h-full">{children}</main>;
-}
--- client/auth/config.ts
+++
@@ -1,6 +0,0 @@
-import { CredentialsProvider, GithubProvider, GoogleProvider } from "@/auth/providers";
-import type { NextAuthConfig } from "next-auth";
-
-export const authConfig = {
-  providers: [CredentialsProvider, GithubProvider, GoogleProvider],
-} satisfies NextAuthConfig;
